7ab0127ada339768423cfa4a305ce42602c21758
jiut <1151386302@qq.com>
2022-06-25 14:34:39 +0800

    暑假前提交

--- rom/替换.py
+++ rom/替换.py
@@ -13,22 +13,21 @@ import re
 
 
 def file_content_refullline(src_txt_file, map, target_txt_file):
-    f1 = open(src_txt_file, 'rb')
+    f1 = open(src_txt_file, 'r', encoding="utf-8")
     lines = f1.readlines()  # 整行读取
     f1.close()
     for line in lines:
-        r1 = line.strip()  # 去除原来每行后面的换行符，但有可能是\r或\r\n
-        fullline = r1.strip().decode('utf-8')
+        fullline = line.strip().strip()  # 去除原来每行后面的换行符，但有可能是\r或\r\n
         matches = re.search('pv_[0-9]{3}.lyric.[0-9]{3}=', fullline)
         if matches is None:
             matches = re.search('pv_[0-9]{3}.song_name=', fullline)
         if matches is not None:
             print(matches.group(0))
-            f2 = open(map, 'rb')
+            f2 = open(map, 'r', encoding="utf-8")
             targetLines = f2.readlines()
             for targetLine in targetLines:
                 r2 = targetLine.strip()
-                target = r2.strip().decode('utf-8')
+                target = r2.strip()
                 lyrics = re.search(r'(?<='+matches.group(0)+').+', target)
                 if lyrics is not None:
                     break
